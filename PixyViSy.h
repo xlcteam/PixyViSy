7f62e2f8a0ccf8ffa56e9709c756cfb6074313a4
mr.Shu <mr@shu.io>
2015-06-27 23:30:13 +0200

    janitoring: Fixed whitespace in PixyViSy.h
    * The codebase should be consistent with regards to whitespace. That is
      what this commit enforeces.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- PixyViSy.h
+++ PixyViSy.h
@@ -1,7 +1,7 @@
 #ifndef _PIXYVISY_H
 #define _PIXYVISY_H
 
-#include <SPI.h>  
+#include <SPI.h>
 #include <Pixy.h>
 #include <stdint.h>
 
@@ -19,11 +19,12 @@ class PixyViSy
     uint8_t getRightPixels(void) { return right_pixels; }
     uint8_t getLeftPixels(void) { return left_pixels; }
     uint8_t getBlocksCount(void) { return blocks_count; }
-	uint8_t getGoalPixHeight(void) { return goal_pix_height; }
+    uint8_t getGoalPixHeight(void) { return goal_pix_height; }
     void setSignature(uint8_t sig) { signature = sig; }
     void setGoalHeight(uint8_t _goal_height) { goal_height = _goal_height; }
-    void setCalibConst(uint16_t calibration_dis_pix) 
-        { if (calibration_dis_pix != 0) calib_const = calibration_dis_pix; }
+    void setCalibConst(uint16_t calibration_dis_pix) {
+        if (calibration_dis_pix != 0) calib_const = calibration_dis_pix;
+    }
     char getAction(void) { return action; }
   private:
     Pixy pixy;
@@ -32,7 +33,7 @@ class PixyViSy
     uint8_t left_pixels;
     uint8_t right_pixels;
     uint8_t goal_height;
-	uint8_t goal_pix_height;
+    uint8_t goal_pix_height;
     uint16_t calib_const;
     char action;
     uint16_t distance;
